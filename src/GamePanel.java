6025f07e478df9119ae44c776f8563c547cedb9a
RediG7 <guleksiredi7@gmail.com>
2022-09-02 23:54:25 +0200

    Develop Paddle, Ball, Score classes & Implement them in GamePanel class

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -7,7 +7,7 @@ import java.util.Random;
 public class GamePanel extends JPanel implements Runnable {
 
     static final int GAME_WIDTH = 1000;
-    static final int GAME_HEIGHT = GAME_WIDTH * (5 / 9);
+    static final int GAME_HEIGHT = (int) (GAME_WIDTH * (0.5555));
     static final Dimension SCREEN_SIZE = new Dimension(GAME_WIDTH, GAME_HEIGHT);
     static final int BALL_DIAMETER = 20;
     static final int PADDLE_WIDTH = 25;
@@ -22,45 +22,99 @@ public class GamePanel extends JPanel implements Runnable {
     Score score;
 
     public GamePanel() {
+        newPaddles();
+        newBall();
+        score = new Score(GAME_WIDTH, GAME_HEIGHT);
+        this.setFocusable(true);
+        this.addKeyListener(new AL());
+        this.setPreferredSize(SCREEN_SIZE);
+
+        gameThread = new Thread(this);
+        gameThread.start();
     }
 
     public void newBall() {
-
+        // random = new Random();
+        ball = new Ball((GAME_WIDTH / 2) - (BALL_DIAMETER / 2), (GAME_HEIGHT / 2) - (BALL_DIAMETER / 2), BALL_DIAMETER, BALL_DIAMETER);
     }
 
     public void newPaddles() {
-
+        paddle1 = new Paddle(0, (GAME_HEIGHT / 2) - (PADDLE_HEIGHT / 2), PADDLE_WIDTH, PADDLE_HEIGHT, 1);
+        paddle2 = new Paddle((GAME_WIDTH - PADDLE_WIDTH), (GAME_HEIGHT / 2) - (PADDLE_HEIGHT / 2), PADDLE_WIDTH, PADDLE_HEIGHT, 2);
     }
 
     public void paint(Graphics g) {
-
+        image = createImage(getWidth(), getHeight());
+        graphics = image.getGraphics();
+        draw(graphics);
+        g.drawImage(image, 0, 0, this);
     }
 
     public void draw(Graphics g) {
-
+        paddle1.draw(g);
+        paddle2.draw(g);
+        ball.draw(g);
     }
 
     public void move() {
-
+        paddle1.move();
+        paddle2.move();
+        ball.move();
     }
 
     public void checkCollision() {
 
+        // bounce ball of top & bottom window edges
+        if (ball.y <= 0) {
+            ball.setYDirection(-ball.yVelocity);
+        }
+        if (ball.y >= GAME_HEIGHT - BALL_DIAMETER) {
+            ball.setYDirection(-ball.yVelocity);
+        }
+
+        // stop paddles at window edges
+        if (paddle1.y <= 0)
+            paddle1.y = 0;
+        if (paddle1.y >= (GAME_HEIGHT - PADDLE_HEIGHT))
+            paddle1.y = GAME_HEIGHT - PADDLE_HEIGHT;
+
+        if (paddle2.y <= 0)
+            paddle2.y = 0;
+        if (paddle2.y >= (GAME_HEIGHT - PADDLE_HEIGHT))
+            paddle2.y = GAME_HEIGHT - PADDLE_HEIGHT;
     }
 
     public void run() {
-
+        // game loop
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1_000_000_000 / amountOfTicks;
+        double delta = 0;
+        while (true) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            if (delta >= 1) {
+                move();
+                checkCollision();
+                repaint();
+                delta--;
+                System.out.println("TEST");
+            }
+        }
     }
 
     public class AL extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
-            super.keyPressed(e);
+            paddle1.keyPressed(e);
+            paddle2.keyPressed(e);
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
-            super.keyReleased(e);
+            paddle1.keyReleased(e);
+            paddle2.keyReleased(e);
         }
     }
 }
